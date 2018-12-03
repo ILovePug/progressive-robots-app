dcfcaeac7be380851fd6d372fa0a1446f1fe1db2
Qian Weng <qweng@almo.com>
2018-12-03 16:48:35 -0500

    Updates

---
+++ precache-manifest.a9f1aabc7afc8617a7a4fa0eb8e665f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/progressive-robots-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c2054f329141b7aba59b",
+    "url": "/progressive-robots-app/static/js/runtime~main.c2054f32.js"
+  },
+  {
+    "revision": "56791034c166957cb1a0",
+    "url": "/progressive-robots-app/static/js/main.56791034.chunk.js"
+  },
+  {
+    "revision": "b154facc2b18ea9aaf2b",
+    "url": "/progressive-robots-app/static/js/1.b154facc.chunk.js"
+  },
+  {
+    "revision": "56791034c166957cb1a0",
+    "url": "/progressive-robots-app/static/css/main.2da9c9b1.chunk.css"
+  },
+  {
+    "revision": "b154facc2b18ea9aaf2b",
+    "url": "/progressive-robots-app/static/css/1.e4f45e5a.chunk.css"
+  },
+  {
+    "revision": "f3b769bddeee165878f223184152dd70",
+    "url": "/progressive-robots-app/index.html"
+  }
+];
